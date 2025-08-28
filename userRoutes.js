bc1ed7c3752ab7085218650d9b77f0ec9dfb33dd
Omega the Great <andrewomega77@gmail.com>
2025-08-28 09:15:38 +0000

    Delete userRoutes.js

--- userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { updateProfile } = require('./userController');
-const { protect, authorize } = require('./authMiddleware');
-const router = express.Router();
-
-router.post('/profile', protect, authorize(['expeditor']), updateProfile);
-
-module.exports = router;
